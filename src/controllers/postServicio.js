20a57eebb1a7584f1f16361871a907942a3c4e11
Agustin Chizzini Melo <aguscn2001@gmail.com>
2024-06-09 20:53:07 -0300

    Update postServicio.js

--- src/controllers/postServicio.js
+++ src/controllers/postServicio.js
@@ -1,33 +1,36 @@
-const postServicios = async(token, titulo, direccion, telefono, horaApertura, minutoApertura, horaCierre, minutoCierre, rubro, descripcion ) => {
- 
-    const formdata = new FormData();
-    // formdata.append("imagenes", fileInput.files[0], "[PROXY]");
-    formdata.append("tituloServicio", titulo);
-    formdata.append("direccion", direccion);
-    formdata.append("telefono", telefono);
-    formdata.append("horaApertura", horaApertura);
-    formdata.append("minutoApertura", minutoApertura);
-    formdata.append("horaCierre", horaCierre);
-    formdata.append("minutoCierre", minutoCierre);
-    formdata.append("rubro", rubro);
-    formdata.append("descripcion", descripcion);
- 
- 
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${token}`);
-    myHeaders.append("Content-Type", "application/json");
- 
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: formdata,
-        redirect: 'follow',
-        mode:'cors'
+const postServicios = async (token, titulo, direccion, telefono, horaApertura, minutoApertura, horaCierre, minutoCierre, rubro, descripcion) => {
+  try {
+      const formdata = new FormData();
+
+      // formdata.append("imagenes", fileInput.files[0], "[PROXY]");
+      formdata.append("tituloServicio", titulo);
+      formdata.append("direccion", direccion);
+      formdata.append("telefono", telefono);
+      formdata.append("horaApertura", horaApertura);
+      formdata.append("minutoApertura", minutoApertura);
+      formdata.append("horaCierre", horaCierre);
+      formdata.append("minutoCierre", minutoCierre);
+      formdata.append("rubro", rubro);
+      formdata.append("descripcion", descripcion);
+
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: formdata,
+          redirect: 'follow',
+          mode: 'cors'
       };
-   
-     
+
       let response = await fetch("https://municipio-g8-servidor-production-dcd2.up.railway.app/api/servicios/post", requestOptions);
-      console.log(response)
-      return response
+      console.log(response);
+      return response;
+  } catch (error) {
+      console.log("error");
+      console.log(error);
+  }
 }
-export default postServicios;
+
+export default postServicios;
